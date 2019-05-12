ec7f54c4901456935668dc53e0ced42a7a684596
ilyes24 <i.ghomrani@esi-sba.dz>
2019-05-12 19:03:39 +0100

    Post and Reaction end point

--- Post/views.py
+++ Post/views.py
@@ -20,7 +20,7 @@ class CommentAPIView(mixins.CreateModelMixin, generics.ListAPIView):
         return qs
 
     def perform_create(self, serializer):
-        serializer.save()
+        serializer.save(comment_owner=self.request.user)
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
@@ -33,3 +33,64 @@ class CommentRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class ReactionAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = ReactionSerializer
+
+    def get_queryset(self):
+        qs = Reaction.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save(reaction_owner=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
+
+class ReactionRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    queryset = Reaction.objects.all()
+    serializer_class = ReactionSerializer
+
+
+class PostAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save(post_owner=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
+
+class PostRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+

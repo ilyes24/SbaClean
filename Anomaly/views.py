ee60f24b31e4fca7014755dd6790941a02c70f11
ilyes24 <i.ghomrani@esi-sba.dz>
2019-05-15 02:49:26 +0100

    Creating Anomaly serializers & CRUD End Point

--- Anomaly/views.py
+++ Anomaly/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.db.models import Q
 
-# Create your views here.
+from .models import Anomaly
+from .serializers import AnomalySerializer
+from rest_framework import mixins, generics
+
+
+class AnomalyAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = AnomalySerializer
+
+    def get_queryset(self):
+        qs = Anomaly.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+        return qs
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {"request": self.request}
+
+
+class AnomalyRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    queryset = Anomaly.objects.all()
+    serializer_class = AnomalySerializer

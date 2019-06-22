c4e80ec80e988c5d409770eab909e049c0545210
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2019-06-22 23:54:35 +0100

    login+register
    finish login to feed and registration processe

---
+++ webapp/form.py
@@ -0,0 +1,22 @@
+from Accounts.models import MyUser
+from Address.models import *
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegistreForm(UserCreationForm):
+    email=forms.EmailField()
+    First_name=forms.CharField(required=True)
+    Last_name=forms.CharField(required=True)
+    phone_number=forms.CharField(max_length=12,required=True)
+    Address=forms.CharField(required=True) 
+    City=forms.ChoiceField(choices=[(City.id, City.name) for City in City.objects.all()])
+   
+    class Meta:
+        model=MyUser
+        fields=['username','email','First_name','Last_name','phone_number','Address','City','password1','password2']
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta():
+        model = MyUser
+        fields = ['username','password']       
--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,13 @@
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.translation import gettext as _
 from django.db import transaction
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .form import *
 
 def index(request):
     context = {}
@@ -19,13 +22,44 @@ def feed(request):
     return render(request, 'feed.html', context)
 
 def login(request):
-    context = {}
-    return render(request, 'login.html', context)
-
+    form=UserForm(request.POST)
+    context = {'form':form}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+                if user.is_active:
+                        auth_login(request,user)
+                        return redirect('feed')
+                else:
+                        return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details given")
+    else:
+             return render(request, 'login.html', context)
+    
 def register(request):
-    context = {}
-    return render(request, 'register.html', context)
+                form=UserRegistreForm(request.POST)
+                context = {'form':form}
+                if form.is_valid():
+                        user = form.save(commit=False)
+                        user.city_id = form.cleaned_data.get('City')
+                        user.save()
+                        raw_password = form.cleaned_data.get('password1')
+                        raw_user=form.cleaned_data.get('username')
+                        user = authenticate(username=raw_user, password=raw_password)
+                        auth_login(request, user)
+                        return redirect('feed')
+                else:
+                        form=UserRegistreForm()
+                        return render(request, 'register.html', context)
+        
+
 
 def post_details(request):
     context = {}
     return render(request, 'post_details.html', context)
+

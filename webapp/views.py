1fde892a5bb71bae9c32bb69896d7c1a94f84147
Aziz <a.attouche@esi-sba.dz>
2019-06-27 01:58:30 +0100

    add social auth views

--- webapp/views.py
+++ webapp/views.py
@@ -8,6 +8,9 @@ from django.db import transaction
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from .form import *
+from social_django.utils import psa, load_strategy
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
 
 def index(request):
     context = {}
@@ -42,8 +45,8 @@ def login(request):
              return render(request, 'login.html', context)
     
 def register(request):
+        if (request.method == 'POST'):
                 form=UserRegistreForm(request.POST)
-                context = {'form':form}
                 if form.is_valid():
                         user = form.save(commit=False)
                         user.city_id = form.cleaned_data.get('City')
@@ -54,9 +57,33 @@ def register(request):
                         auth_login(request, user)
                         return redirect('feed')
                 else:
+                        print(form)
+                        return redirect('register')
+        else:
+                partial_token = None
+                if request.GET.get('partial_token'):
+                        strategy = load_strategy()
+                        partial_token = request.GET.get('partial_token')
+                        partial = strategy.partial_load(partial_token)
+                        data = partial.data['kwargs']['details']
+                        form=UserRegistreForm(initial = {'username': data['username'],'email' :data['email'], 'First_name': data['first_name'],'Last_name': data['last_name']})
+                        context = {'form':form}
+                        return render(request, 'register.html', context)
+                else:
                         form=UserRegistreForm()
+                        context = {'form':form}
                         return render(request, 'register.html', context)
         
+def social_auth(request):
+        strategy = load_strategy()
+        partial_token = request.GET.get('partial_token')
+        partial = strategy.partial_load(partial_token)
+
+        email = partial.data['kwargs']['details']['email']
+        user = UserModel.objects.get(email=email)
+        auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        return redirect('feed')
+
 
 
 def post_details(request):

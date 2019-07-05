c3e4f9dbc1d32e9c75e06d35658ef774c14510d4
Nadir Bounoua <anbounoua@gmail.com>
2019-07-05 21:38:36 +0100

    Added Post to admin

--- Post/admin.py
+++ Post/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import *
 
-# Register your models here.
+admin.site.register(Post)

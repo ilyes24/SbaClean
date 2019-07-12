faa5f555e9aa4eedfce83c3642180d9dab402b9d
Nadir Bounoua <anbounoua@gmail.com>
2019-07-12 17:11:48 +0100

    Added urls for dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,8 @@ from dashboard import views as dash_views
 app_name = "dashboard"
 
 urlpatterns = [
-    path('dashboard', dash_views.dashboard_index, name="dashboard_index")
+    path('dashboard', dash_views.dashboard_index, name="dashboard"),
+    path('dashboard/anomalies', dash_views.dashboard_anomalies, name="dashboard_anomalies"),
+    path('dashboard/anomalies/edit/<int:pid>', dash_views.dashboard_anomalie_edit, name="dashboard_anomalie_edit"),
+    path('dashboard/users', dash_views.dashboard_users, name="dashboard_users"),
 ]

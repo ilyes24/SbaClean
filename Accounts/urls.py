83c8c197b9fa7c81c5c5c42f35faf5dc83239da8
ilyes24 <i.ghomrani@esi-sba.dz>
2019-08-13 18:37:34 +0200

    add UserRanking url

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from Accounts.Api.api import ListUserView, CreateUserView, UserRUView
+from Accounts.Api.api import ListUserView, CreateUserView, UserRUView, UserRanking
 
 
 api_urlpatterns = [
     path('', ListUserView.as_view(), name='user-list'),
     path('register/', CreateUserView.as_view(), name='user-create'),
     path('<pk>', UserRUView.as_view(), name='user-ru'),
+    path('ranking', UserRanking.as_view(), name='user-ranking')
 ]
 
 urlpatterns = [

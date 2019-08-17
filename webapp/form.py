0733129b2f9a2c07f2765828ff561f4ca08cc14d
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2019-08-17 21:40:30 +0100

    supp city  du form

--- webapp/form.py
+++ webapp/form.py
@@ -11,11 +11,11 @@ class UserRegistreForm(UserCreationForm):
     last_name=forms.CharField(required=True)
     phone_number=forms.CharField(required=True)
     address=forms.CharField(required=True) 
-    City=forms.ChoiceField(choices=[(City.id, City.name) for City in City.objects.all()])
+    #City=forms.ChoiceField(choices=[(City.id, City.name) for City in City.objects.all()])
    
     class Meta:
         model=MyUser
-        fields=['username','email','first_name','last_name','phone_number','address','City','password1','password2']
+        fields=['username','email','first_name','last_name','phone_number','address','password1','password2']
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta():

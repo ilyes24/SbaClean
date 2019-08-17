5312a5e704611ae905b524d24ea47f626d2b17cf
Nadir Bounoua <anbounoua@gmail.com>
2019-08-17 16:06:52 +0100

    Major updates to dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,6 +7,8 @@ from django.db import transaction
 from .models import *
 from .form import *
 from Post.models import *
+from Anomaly.models import *
+from Address.models import *
 from Accounts.models import *
 from Event.models import *
 import json 
@@ -27,7 +29,7 @@ menu = [
     {
     "title": "Evénements",
     "status": "",
-    "url": "dashboard:dashboard",
+    "url": "dashboard:dashboard_events",
     "icon": "calendar_today"
     },
     {
@@ -39,13 +41,13 @@ menu = [
     {
     "title": "Commentaires",
     "status": "",
-    "url": "dashboard:dashboard",
+    "url": "dashboard:dashboard_comments",
     "icon": "comment"
     },
     {
     "title": "Signalements",
     "status": "",
-    "url": "dashboard:dashboard",
+    "url": "dashboard:dashboard_reports",
     "icon": "info"
     }
 ]
@@ -69,8 +71,8 @@ def dashboard_anomalies(request):
         if action == 'delete':
             post_id = request.POST.get('post_id')
             posts = Post.objects.filter(id=post_id).delete()
-    posts = Post.objects.all()
-    context = {"menu": menu, "posts": posts }
+    anomalies = Anomaly.objects.all()
+    context = {"menu": menu, "anomalies": anomalies }
     return render(request, 'anomalies.html', context)
 
 def dashboard_anomalie_edit(request, pid):
@@ -95,17 +97,59 @@ def dashboard_users(request):
     context = {'menu': menu, 'users': users}
     return render(request, 'users.html', context)
 
+def dashboard_users_edit(request):
+    menu_active('Utilisateurs')
+    if request.method == 'POST' and request.POST.get('action') == 'delete':
+        user_id = request.POST.get('user_id')
+        users = MyUser.objects.filter(id=user_id).delete()
+    users = MyUser.objects.all()
+    context = {'menu': menu, 'users': users}
+    return render(request, 'users.html', context)
+
 def dashboard_events(request):
     menu_active('Evénements')
-    context = {}
-    return render(request, 'index_dashboard.html', context)
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'delete':
+            post_id = request.POST.get('post_id')
+            posts = Post.objects.filter(id=post_id).delete()
+    events = Event.objects.all()
+    context = {"menu": menu, "events": events }
+    return render(request, 'events.html', context)
+
+def dashboard_event_approve(request, eid):
+    event = Event.objects.filter(id=eid)[0].approve()
+    return redirect('dashboard:dashboard_events')
+
+def dashboard_events_edit(request):
+    menu_active('Evénements')
+    context = {'menu': menu, }
+    return render(request, 'events.html', context)
 
 def dashboard_comments(request):
+    menu_active('Commentaires') 
+    if request.method == 'POST' and request.POST.get('action') == 'delete':
+        comment_id = request.POST.get('comment_id')
+        Comment.objects.filter(id=user_id).delete()
+    comments = Comment.objects.all()
+    context = {'menu': menu, 'comments': comments}
+    return render(request, 'comments.html', context)
+
+def dashboard_comments_edit(request):
     menu_active('Commentaires')    
-    context = {}
-    return render(request, 'index_dashboard.html', context)
+    context = {'menu': menu, }
+    return render(request, 'comments.html', context)
 
 def dashboard_reports(request):
     menu_active('Signalements')
-    context = {}
-    return render(request, 'index_dashboard.html', context)
+    if request.method == 'POST' and request.POST.get('action') == 'delete':
+        user_id = request.POST.get('user_id')
+        users = MyUser.objects.filter(id=user_id).delete()
+    users = MyUser.objects.all()    
+    context = {'menu': menu, 'reports': reports}
+    return render(request, 'reports.html', context)
+
+def dashboard_reports_edit(request):
+    menu_active('Signalements')
+    context = {'menu': menu, }
+    return render(request, 'reports.html', context)

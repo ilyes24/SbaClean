78a6c63e1c0e6f9e97cc2c5b1a10effaab7b5139
ilyes24 <i.ghomrani@esi-sba.dz>
2019-08-31 17:56:50 +0100

    fix updating user profile

---
+++ SetUpDB.sql
@@ -0,0 +1,6 @@
+INSERT INTO "Address_state" ("code", "name") VALUES (22, 'Sidi Bel Abbes');
+INSERT INTO "Address_city" (zip_code, name, state_id) VALUES (22001, 'Sidi Bel Abbes Adim Fatiha', 1);
+INSERT INTO "Address_city" (zip_code, name, state_id) VALUES (22002, 'Sidi Bel Abbes Chouhada', 1);
+INSERT INTO "Address_city" (zip_code, name, state_id) VALUES (22003, 'Sidi Bel Abbes Khelladi', 1);
+INSERT INTO "Address_city" (zip_code, name, state_id) VALUES (22004, 'Sidi Bel Abbes Larbi ben M''hidi', 1);
+INSERT INTO "Address_city" (zip_code, name, state_id) VALUES (22005, 'Sidi Bel Abbes 1er Novembre', 1);

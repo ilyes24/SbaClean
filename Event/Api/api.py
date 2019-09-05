1f46547b8c3daab58ec889672d5f2e7177b79fdf
ilyes24 <i.ghomrani@esi-sba.dz>
2019-09-05 13:49:52 +0100

    add felttering by event in participation

--- Event/Api/api.py
+++ Event/Api/api.py
@@ -62,31 +62,11 @@ class EventParticipationAPIView(mixins.CreateModelMixin, generics.ListAPIView):
     def get_queryset(self):
         qs = EventParticipation.objects.all()
 
-        query_post = self.request.GET.get("post")
-        query_date = self.request.GET.get("date")
-        query_owner = self.request.GET.get("owner")
-        query_title = self.request.GET.get("title")
-        query_city = self.request.GET.get("city")
-        query_description = self.request.GET.get("description")
+        query_event = self.request.GET.get("event")
         query_user = self.request.GET.get("user")
 
-        if query_post is not None:
-            qs = qs.filter(Q(event__post__exact=query_post)).distinct()
-
-        if query_date is not None:
-            qs = qs.filter(Q(event__starts_at__lte=query_date)).distinct()
-
-        if query_owner is not None:
-            qs = qs.filter(Q(event__post__post_owner__exact=query_owner)).distinct()
-
-        if query_title is not None:
-            qs = qs.filter(Q(event__post__title__exact=query_title)).distinct()
-
-        if query_city is not None:
-            qs = qs.filter(Q(event__post__city__exact=query_city)).distinct()
-
-        if query_description is not None:
-            qs = qs.filter(Q(event__post__description__contains=query_description)).distinct()
+        if query_event is not None:
+            qs = qs.filter(Q(event__post__exact=query_event)).distinct()
 
         if query_user is not None:
             qs = qs.filter(Q(user__exact=query_user)).distinct()

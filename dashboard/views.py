522350a1f93ac44de7461b13ccc3ccd58167c939
Nadir Bounoua <anbounoua@gmail.com>
2019-09-06 19:10:27 +0100

    Dashboard only for staff

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,6 +11,7 @@ from Anomaly.models import *
 from Address.models import *
 from Accounts.models import *
 from Event.models import *
+from django.contrib.admin.views.decorators import staff_member_required
 import json 
 
 menu = [
@@ -65,6 +66,7 @@ def menu_active(title):
         else:
             item["status"] = ""
 
+@staff_member_required
 def dashboard_index(request):
     menu_active('Dashboard')
     users_count = MyUser.objects.all().count()
@@ -89,6 +91,7 @@ def dashboard_index(request):
     context = {"menu": menu, "stats": stats, "anomalies": anomalies}
     return render(request, 'index_dashboard.html', context)
 
+@staff_member_required
 def dashboard_anomalies(request):
     menu_active('Anomalies')
     if request.method == 'POST':
@@ -100,6 +103,7 @@ def dashboard_anomalies(request):
     context = {"menu": menu, "anomalies": anomalies }
     return render(request, 'anomalies.html', context)
 
+@staff_member_required
 def dashboard_anomalie_edit(request, pid):
     menu_active('Anomalies')
     post = Post.objects.get(id=pid)
@@ -113,12 +117,14 @@ def dashboard_anomalie_edit(request, pid):
     context = {'menu': menu, 'post': post, 'form': form}
     return render(request, 'anomalie_edit.html', context)
 
+@staff_member_required
 def dashboard_anomalie_archive(request, pid):
     menu_active('Anomalies')
     anomaly = Anomaly.objects.get(post=pid)
     anomaly.archive()
     return redirect('dashboard:dashboard_anomalies')
 
+@staff_member_required
 def dashboard_archives(request):
     menu_active('Anomalies archivées')
     if request.method == 'POST':
@@ -130,6 +136,7 @@ def dashboard_archives(request):
     context = {"menu": menu, "anomalies": anomalies }
     return render(request, 'archives.html', context)
 
+@staff_member_required
 def dashboard_users(request):
     menu_active('Utilisateurs')
     if request.method == 'POST' and request.POST.get('action') == 'ban':
@@ -139,11 +146,13 @@ def dashboard_users(request):
     context = {'menu': menu, 'users': users}
     return render(request, 'users.html', context)
 
+@staff_member_required
 def dashboard_users_edit(request):
     menu_active('Utilisateurs')
     context = {'menu': menu}
     return render(request, 'users.html', context)
 
+@staff_member_required
 def dashboard_events(request):
     menu_active('Evénements')
     if request.method == 'POST':
@@ -155,15 +164,18 @@ def dashboard_events(request):
     context = {"menu": menu, "events": events }
     return render(request, 'events.html', context)
 
+@staff_member_required
 def dashboard_event_approve(request, eid):
     event = Event.objects.filter(id=eid)[0].approve()
     return redirect('dashboard:dashboard_events')
 
+@staff_member_required
 def dashboard_events_edit(request):
     menu_active('Evénements')
     context = {'menu': menu, }
     return render(request, 'events.html', context)
 
+@staff_member_required
 def dashboard_comments(request):
     menu_active('Commentaires') 
     if request.method == 'POST' and request.POST.get('action') == 'delete':
@@ -173,11 +185,13 @@ def dashboard_comments(request):
     context = {'menu': menu, 'comments': comments}
     return render(request, 'comments.html', context)
 
+@staff_member_required
 def dashboard_comments_edit(request):
     menu_active('Commentaires')    
     context = {'menu': menu, }
     return render(request, 'comments.html', context)
 
+@staff_member_required
 def dashboard_reports(request):
     menu_active('Signalements')
     if request.method == 'POST' and request.POST.get('action') == 'ban':
@@ -192,6 +206,7 @@ def dashboard_reports(request):
     context = {'menu': menu, 'reports': reports}
     return render(request, 'reports.html', context)
 
+@staff_member_required
 def dashboard_reports_edit(request):
     menu_active('Signalements')
     context = {'menu': menu, }

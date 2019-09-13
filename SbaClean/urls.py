110ac486d73e347fab3bf2a6a2bff8f423b2262d
ilyes24 <i.ghomrani@esi-sba.dz>
2019-09-13 16:15:17 +0100

    prepare for deployment

--- SbaClean/urls.py
+++ SbaClean/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_swagger.views import get_swagger_view
 from django.contrib.auth import views as auth_views
-from webapp.views import UploadView
-from webapp import views
+# from webapp.views import UploadView
+# from webapp import views
 from django.conf.urls import handler404, handler500
 
 schema_view = get_swagger_view(title='SbaClean API')
@@ -37,37 +37,37 @@ api_urlpatterns = [
 ]
 
 urlpatterns = [
-                  path('', views.index),
-                  path('login/', views.login, name='login'),
-                  path('accounts/login/', views.login, name='login'),
-                  path('register/', views.register, name='register'),
-                  path('feed/', views.feed, name='feed'),
-                  path('event/', views.event, name='event'),
-                  path('profile/', views.profile, name='profile'),
-                  path('post_details/', views.feed, name='post_details'),
-                  path('social-auth/', views.social_auth, name='social_auth'),
-                  path('', include('social_django.urls', namespace='social')),
-                  path("logout/", views.logout, name="logout"),
-                  path('', include('dashboard.urls', namespace='dashboard')),
-                  path('api/upload-image', UploadView.as_view()),
+                  # path('', views.index),
+                  # path('login/', views.login, name='login'),
+                  # path('accounts/login/', views.login, name='login'),
+                  # path('register/', views.register, name='register'),
+                  # path('feed/', views.feed, name='feed'),
+                  # path('event/', views.event, name='event'),
+                  # path('profile/', views.profile, name='profile'),
+                  # path('post_details/', views.feed, name='post_details'),
+                  # path('social-auth/', views.social_auth, name='social_auth'),
+                  # path('', include('social_django.urls', namespace='social')),
+                  # path("logout/", views.logout, name="logout"),
+                  # path('', include('dashboard.urls', namespace='dashboard')),
+                  # path('api/upload-image', UploadView.as_view()),
 
-
-    path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    #
+    # path('admin/', admin.site.urls),
+    # path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/v1/', include(api_urlpatterns)),
-    url(r'^rest-auth/', include('rest_auth.urls')),
-    url(r'^like/$',views.like_post,name="like_post"),
-    url(r'^dislike/$',views.dislike_post,name="dislike_post"),
-    url(r'^signaled/$',views.signaled,name="signaled"),
-    url(r'^Myposts/$',views.Myposts,name="Myposts"),
-    url(r'^Myreactions/$',views.Myreactions,name="Myreactions"),
-    url(r'^comment_delete/$',views.comment_delete,name="comment_delete"),
-    url(r'^create_comment/$',views.create_comment,name="create_comment"),
-    url(r'^Participate/$',views.Participate,name="Participate")
+    # url(r'^rest-auth/', include('rest_auth.urls')),
+    # url(r'^like/$',views.like_post,name="like_post"),
+    # url(r'^dislike/$',views.dislike_post,name="dislike_post"),
+    # url(r'^signaled/$',views.signaled,name="signaled"),
+    # url(r'^Myposts/$',views.Myposts,name="Myposts"),
+    # url(r'^Myreactions/$',views.Myreactions,name="Myreactions"),
+    # url(r'^comment_delete/$',views.comment_delete,name="comment_delete"),
+    # url(r'^create_comment/$',views.create_comment,name="create_comment"),
+    # url(r'^Participate/$',views.Participate,name="Participate")
     
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
-handler404 = views.error_404
-handler500 = views.error_500
+#
+#
+# handler404 = views.error_404
+# handler500 = views.error_500

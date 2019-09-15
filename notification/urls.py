894aa84166b315bce194b38e7597478d694c6482
Aziz <a.attouche@esi-sba.dz>
2019-09-15 04:13:11 +0100

    add new-user notification

--- notification/urls.py
+++ notification/urls.py
@@ -6,6 +6,8 @@ app_name = 'notification'
 urlpatterns = [
     path('', views.index, name="index"),
     #path('check_new_posts/', views.update_posts_notification, name ="check_new_posts"),
-    path('send_notification/', views.send_new_anomaly_notification, name ="send_new_anomaly_notification")
+    path('send_notification/', views.send_new_anomaly_notification, name ="send_new_anomaly_notification"),
+    path('new-user/', views.new_user, name ="new-user")
+
 
 ]

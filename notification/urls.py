7db57aac88a36333f8e5afa139dff42e184ec656
Aziz <a.attouche@esi-sba.dz>
2019-09-15 04:57:43 +0100

    add check new anomalys to notification app

--- notification/urls.py
+++ notification/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'notification'
 urlpatterns = [
     path('', views.index, name="index"),
-    #path('check_new_posts/', views.update_posts_notification, name ="check_new_posts"),
+    path('check_new_posts/', views.update_posts_notification, name ="check_new_posts"),
     path('send_notification/', views.send_new_anomaly_notification, name ="send_new_anomaly_notification"),
     path('new-user/', views.new_user, name ="new-user")
 
--- notification/views.py
+++ notification/views.py
@@ -34,7 +34,8 @@ def update_posts_notification(request):
 
     if request.method == 'POST':
         count_old = request.POST['count']
-        count_new = Anomaly.objects.count()
+        city = request.POST['city']
+        count_new = Anomaly.objects.filter(post__city = city).count()
 
         if (count_new > int(count_old)):
             context = { "changed" : True }
